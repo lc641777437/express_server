14c32e7a96f955b7a21baf9a249c7c172c7395a3
piggysticker <641777437@qq.com>
2017-03-26 23:20:59 +0800

    nothing

--- routes/contact.js
+++ routes/contact.js
@@ -7,7 +7,7 @@ var sendMail = require('../sendMail');
 /* GET home page. */
 router.post('/', function(req, res, next) {
 	log.info("sendMail");
-	sendMail('lc523@hust.edu.cn', 'Blog_Contact', req.body);
+	sendMail('lc523@hust.edu.cn', 'Blog_Contact', '<p>' + req.body + '</p>');
     res.render('index', { title: 'Express' });
 });
 
