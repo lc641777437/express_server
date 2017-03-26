08ad94f2aeccdc95d36bb8a8a80653c025dabdd2
piggysticker <641777437@qq.com>
2017-03-26 22:16:00 +0800

    增加sendMail

---
+++ sendMail.js
@@ -0,0 +1,28 @@
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var config = require('../../config');
+var log = require('../../logger');
+
+smtpTransport = nodemailer.createTransport(smtpTransport({
+    service: config.email.service,
+    auth: {
+        user: config.email.user,
+        pass: config.email.pass
+    }
+}));
+
+var sendMail = function (recipient, subject, html) {
+    smtpTransport.sendMail({
+        from: config.email.user,
+        to: recipient,
+        subject: subject,
+        html: html
+    }, function (error, response) {
+        if (error) {
+            log.error(error);
+        }
+        log.info('sendMail success.')
+    });
+}
+
+module.exports = sendMail;
